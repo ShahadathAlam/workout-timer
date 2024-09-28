70d8d6ea4a553602109ee7fdcbbfded73451253b
ShahadathAlam <shahadathalam@ymail.com>
2024-09-29 00:48:26 +0600

    Closures in Effects

--- src/Calculator.js
+++ src/Calculator.js
@@ -27,6 +27,13 @@ function Calculator({ workouts, allowSound }) {
     [number, sets, speed, durationBreak]
   );
 
+  useEffect(
+    function () {
+      console.log(duration, sets);
+      document.title = `Your ${number}-exercise workout`;
+    },
+    [number, duration, sets]
+  );
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
